2afa2e7f04106e441b7c3d4843aa970fb9fe66ae
izrarya <op286@fifgroup.co.id>
2025-04-24 15:08:24 +0700

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-function App() {
-  return (
-    <>
-      <div className="text-3xl">
-        Hello World
-      </div>
-    </>
-  )
-}
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import InstructionsPage from './pages/IntructionsPage';
+import GamePage from './pages/GamePage';
+import ResultPage from './pages/ResultPage';
 
-export default App
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/instructions" element={<InstructionsPage />} />
+        <Route path="/game" element={<GamePage />} />
+        <Route path="/result" element={<ResultPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
